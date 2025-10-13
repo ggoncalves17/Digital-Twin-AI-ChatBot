f6f37b2ee1cc9010994212e259d2adf3e865ec40
Rodrigo Machado <29443508+ralmachado@users.noreply.github.com>
2025-10-13 12:30:01 +0100

    feat: add Persona schemas

--- src/digital_twin/schemas/__init__.py
+++ src/digital_twin/schemas/__init__.py
@@ -1,3 +1,7 @@
-"""
-Pydantic schemas.
-"""
+"""Pydantic schemas."""
+
+from .education import Education
+from .hobby import Hobby
+from .occupation import Occupation
+
+__all__ = ["Education", "Hobby", "Occupation"]
---
+++ src/digital_twin/schemas/persona.py
@@ -0,0 +1,82 @@
+from datetime import date
+from enum import Enum
+from typing import Annotated, ClassVar
+
+from pydantic import AfterValidator, BaseModel, ConfigDict, Field
+
+from digital_twin.schemas import Education, Hobby, Occupation
+
+
+class GenderEnum(Enum):
+    MALE = "Male"
+    FEMALE = "Female"
+    OTHER = "Other"
+
+
+def validate_birthdate(v: date) -> date:
+    if date.today() < v:
+        raise ValueError("Birthdate cannot be in the future.")
+    return v
+
+
+class PersonaBase(BaseModel):
+    """Base Persona schema."""
+
+    name: str = Field(min_length=1, max_length=100, description="Persona's name")
+    birthdate: Annotated[date, AfterValidator(validate_birthdate)] = Field(
+        description="Persona's birthdate"
+    )
+    gender: GenderEnum = Field(description="Persona's gender")
+    nationality: str = Field(description="Persona's nationality")
+    education: list[Education] = Field(
+        default_factory=list, description="Persona's academic career"
+    )
+    occupations: list[Occupation] = Field(
+        default_factory=list, description="Persona's work experience"
+    )
+    hobbies: list[Hobby] = Field(
+        default_factory=list, description="Persona's hobbies"
+    )
+
+
+class PersonaCreate(PersonaBase):
+    """Persona creation schema."""
+
+    pass
+
+
+class PersonaUpdate(BaseModel):
+    """Persona update schema."""
+
+    name: str | None = Field(
+        None, min_length=1, max_length=100, description="Persona name"
+    )
+    birthdate: Annotated[date | None, AfterValidator(validate_birthdate)] = Field(None)
+    gender: GenderEnum | None = Field(None)
+    nationality: str | None = Field(None)
+    education: list[Education] | None = Field(None)
+    occupations: list[Occupation] | None = Field(None)
+    hobbies: list[Hobby] | None = Field(None)
+
+
+class Persona(PersonaBase):
+    """Persona response schema."""
+
+    id: Annotated[int, Field(description="Unique Persona ID")]
+
+    model_config: ClassVar[ConfigDict] = ConfigDict(
+        from_attributes=True,
+        json_schema_extra={
+            "examples": {
+                "id": 1,
+                "name": "John Doe",
+                "birthdate": "2000-01-31",
+                "gender": "Male",
+                "nationality": "Portuguese",
+                "education": [],
+                "occupations": [],
+                "hobbies": [],
+            }
+        },
+    )
+
