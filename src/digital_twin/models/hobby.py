4a33efd0c0cf0aab4138f60e1ed8aecf3ee67b3a
Utilizador <guilherme.sousa@theloop.pt>
2025-10-13 15:33:15 +0100

    type change of the column, and relationship Persona

--- src/digital_twin/models/hobby.py
+++ src/digital_twin/models/hobby.py
@@ -1,8 +1,7 @@
 """SQLalchemy Model for hobby."""
 
-from unittest.mock import Base
 from src.digital_twin.schemas.hobby import HobbyFrequency, HobbyType
-from sqlalchemy import Column, Enum, Integer, String
+from sqlalchemy import Column, Integer, String, Base, ForeignKey, relationship
 
 class Hobby(Base):
     """SQLAlchemy model representing a hobby."""
@@ -10,12 +9,13 @@ class Hobby(Base):
     __tablename__ = "hobbies"
 
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
-    type = Column(Enum(HobbyType), nullable=False)
+    type = Column(String(100), nullable=False)
     name = Column(String(100), nullable=False)
-    freq = Column(Enum(HobbyFrequency), nullable=False)
+    freq = Column(String(100), nullable=False)
     persona_id = Column(Integer, ForeignKey("personas.id"), nullable=False)
+    persona = relationship("Persona", back_populates="hobbies")
     # Add any additional fields or relationships as necessary
     
 
     def __repr__(self) -> str:
-        return f"<Hobby(id={self.id}, type={self.type}, name={self.name}, freq={self.freq})>"
+        return f"<Hobby(id={self.id}, type={self.type}, name={self.name}, freq={self.freq}, persona_id={self.persona_id})>"
