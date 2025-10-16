550b274e56d2cd8ff1d8b1890353be193ddbf01c
Utilizador <guigonzo.1@hotmail.com>
2025-10-16 11:26:49 +0100

    feat: add langchain chat endpoint with logic

---
+++ src/digital_twin/routers/questions_answers.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends, HTTPException, Query, status
+from digital_twin.services.questions_answers import QAService
+from typing import Annotated
+from sqlalchemy.orm import Session
+from digital_twin.database import get_db
+from digital_twin.schemas.question_answer import QACreate
+
+router = APIRouter(prefix="/chat", tags=["chat"])
+
+@router.post("/")
+def questions_answers(
+    question: QACreate,
+    db: Annotated[Session, Depends(get_db)],
+):
+    result = QAService.generate_response(db, question)
+
+    if result is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Persona id not found"
+        )    
+    return result
