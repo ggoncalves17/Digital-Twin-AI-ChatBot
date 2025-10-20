1c17d53da79b5eed964b17b36268a37eab16289c
Guilherme Gonçalves <134335469+ggoncalves17@users.noreply.github.com>
2025-10-20 10:03:20 +0100

    feat: add User and Chat schemas (#53)
    * feat: add User schemas
    
    * refactor: removed unused imports
    
    * fix: fixed field 'name' on UserUpdate
    
    * feat: add Chat schemas
    
    * feat: add 'chats' field on User json response example
    
    * refactor: changed some fields on Chat and ChatMessage schemas

--- src/digital_twin/schemas/__init__.py
+++ src/digital_twin/schemas/__init__.py
@@ -3,5 +3,7 @@
 from .education import Education
 from .hobby import Hobby
 from .occupation import Occupation
+from .chat_message import ChatMessage
+from .chat import Chat
 
-__all__ = ["Education", "Hobby", "Occupation"]
+__all__ = ["Education", "Hobby", "Occupation", "ChatMessage", "Chat"]
---
+++ src/digital_twin/schemas/user.py
@@ -0,0 +1,69 @@
+"""
+User data models
+"""
+
+import re
+from datetime import date
+from typing import Annotated, ClassVar
+
+from pydantic import AfterValidator, BaseModel, ConfigDict, EmailStr, Field
+
+from digital_twin.schemas import Chat
+
+def validate_birthdate(v: date) -> date:
+    if date.today() < v:
+        raise ValueError("Birthdate cannot be in the future.")
+    return v
+
+
+def validate_password(v):
+    if not re.search(r"[A-Z]", v):
+        raise ValueError("Password must contain at least one uppercase letter")
+    if not re.search(r"[a-z]", v):
+        raise ValueError("Password must contain at least one lowercase letter")
+    if not re.search(r"\d", v):
+        raise ValueError("Password must contain at least one digit")
+    return v
+
+
+class UserBase(BaseModel):
+    """Base User schema."""
+
+    name: str = Field(min_length=1, max_length=100, description="User's name")
+    birthdate: Annotated[date, AfterValidator(validate_birthdate)] = Field(
+        description="User's birthdate"
+    )
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    """User creation schema."""
+
+    password: Annotated[str, AfterValidator(validate_password)] = Field(
+        min_length=8, description="User's Password"
+    )
+
+
+class UserUpdate(BaseModel):
+    """User update schema."""
+
+    name: str | None = Field(None, min_length=1, max_length=100, description="User's name")
+
+class User(UserBase):
+    """User response schema."""
+
+    id: Annotated[int, Field(description="Unique User ID")]
+    chats: list[Chat] = Field(description="User's chats")
+
+    model_config: ClassVar[ConfigDict] = ConfigDict(
+        from_attributes=True,
+        json_schema_extra={
+            "example": {
+                "id": 1,
+                "name": "John Doe",
+                "birthdate": "2000-01-31",
+                "email": "john.doe@gmail.com",
+                "chats": [],
+            }
+        },
+    )
