56d11a39b39665430b839f074d213752cf81917c
Guilherme Gonçalves <guigonzo.1@hotmail.com>
2025-10-23 23:06:29 +0100

    fix: frontend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
+  baseURL: "https://digital-twin-latest.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
